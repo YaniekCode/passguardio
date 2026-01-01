ef9b7da12581452ec4b64cee619c676af58f2612
YaniekCode <yaniek@duck.com>
2026-01-01 14:42:35 +0100

    Revert "Added Dockerfile and .dockerignore files and edited the DB path"
    This reverts commit 0d2faaab1b3b74d9df1e516e272a4636430831a6.
    
    	deleted:    .dockerignore
    	deleted:    Dockerfile
    	renamed:    data/mydb.db -> src/api/db/mydb.db
    	modified:   src/api/db/openDb.ts

--- src/api/db/openDb.ts
+++ src/api/db/openDb.ts
@@ -1,16 +1,14 @@
-import path from 'node:path';
 import sqlite3 from 'sqlite3';
 import { open, Database } from 'sqlite';
 
 sqlite3.verbose();
 
 export default async function openDb(): Promise<Database> {
-	const dbPath = path.join('./data', 'mydb.db');
+  const db = await open({
+    filename: './src/api/db/mydb.db',
+    driver: sqlite3.Database,
+  });
 
-  	const db = await open({
-    		filename: dbPath,
-    		driver: sqlite3.Database,
-  	});
-  	return db;
+  return db;
 }
 
